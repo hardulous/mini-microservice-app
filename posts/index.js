ab30689f61cc8dcb319d75f6443853fd0e87368b
hardulous <bishtaman120@gmail.com>
2025-02-10 23:20:35 +0530

    added services

--- posts/index.js
+++ posts/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
+const cors = require('cors')
 
 const app = express();
 
-app.use([express.json(), express.urlencoded()]);
+app.use([cors(),express.json(), express.urlencoded()]);
 
 // database
 const posts = {};
