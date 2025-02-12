5934b6d3b72855aad79cd80c8d22102e4451df23
hardulous <bishtaman120@gmail.com>
2025-02-12 23:18:24 +0530

    added query service

---
+++ event-bus/index.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use([cors(), express.json(), express.urlencoded()]);
+
+// A post handler where request come from microservice to tell event bus about the event
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  // Here whatever event is coming pass it down to all microservices
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+
+  res.send({ status: "Ok" });
+});
+
+app.listen(4005, () => console.log("Listening on 4005"));
